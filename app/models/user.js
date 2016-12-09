9779597629d4ff3b9326776f05c4088939d437c3
Mahmood Khan <mkhan@mercycorps.org>
2016-12-08 20:18:00 -0800

    added user model

---
+++ app/models/user.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    username: DS.attr('string'),
+    password: DS.attr('string'),
+    email: DS.attr('string'),
+    first_name: DS.attr('string'),
+    last_name: DS.attr('string')
+});
