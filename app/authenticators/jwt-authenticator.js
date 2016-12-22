a5489a8be1546c177b15826f0e36790ec96a30c7
Mahmood Khan <mkhan@mercycorps.org>
2016-12-22 07:43:58 -0800

    added a reject promise

--- app/authenticators/jwt-authenticator.js
+++ app/authenticators/jwt-authenticator.js
@@ -1,27 +1,34 @@
 import Ember from 'ember';
 import Base from 'ember-simple-auth/authenticators/base';
 import Cookies from 'ember-cli-js-cookie';
-import ENV from 'frontendapp/config/environment';
 
 export default Base.extend({
-  restore(data) {
-    return new Ember.RSVP.Promise((resolve, reject) => {
-      if (!Ember.isEmpty(data.token)) {
-        resolve(data);
-      } else {
-        reject();
-      }
-    });
-  },
 
-  authenticate() {
-    return new Ember.RSVP.Promise((resolve, reject) => {
-        var jwt = Cookies.get('jwt');
-        Ember.run(function() {
-          resolve({
-            token: jwt
-          });
+    restore(data) {
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            if (!Ember.isEmpty(data.token)) {
+                resolve(data);
+            } else {
+                reject();
+            }
         });
-    });
-  },
+    },
+
+    authenticate() {
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            var jwt = Cookies.get('jwt');
+            if (typeof(jwt) !== 'undefined') {
+                Cookies.remove('jwt');
+                Ember.run(function() {
+                    resolve({
+                        token: jwt
+                    });
+                });
+            } else {
+                Ember.run(function() {
+                    reject();
+                });
+            }
+        });
+    },
 });
--- app/authorizers/drf-token-authorizer.js
+++ app/authorizers/drf-token-authorizer.js
@@ -5,7 +5,6 @@ export default Base.extend({
   session: Ember.inject.service('session'),
 
   authorize: function(sessionData, block) {
-    console.log(sessionData);
     if (this.get('session.isAuthenticated') && !Ember.isEmpty(sessionData.token)) {
       block('Authorization', 'JWT ' + sessionData.token);
     }
