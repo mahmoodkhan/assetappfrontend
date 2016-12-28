803333e6edb44b45dfd5b167ddb071267b18f6cb
Mahmood Khan <mkhan@mercycorps.org>
2016-12-28 07:29:46 -0800

    made the login page look nicer

--- app/authenticators/drf-token-authenticator.js
+++ app/authenticators/drf-token-authenticator.js
@@ -3,40 +3,39 @@ import Base from 'ember-simple-auth/authenticators/base';
 import ENV from 'frontendapp/config/environment';
 
 export default Base.extend({
-  restore(data) {
-    return new Ember.RSVP.Promise((resolve, reject) => {
-      if (!Ember.isEmpty(data.token)) {
-        resolve(data);
-      } else {
-        reject();
-      }
-    });
-  },
-
-  authenticate(username, password) {
-    return new Ember.RSVP.Promise((resolve, reject) => {
-      Ember.$.ajax({
-        url: ENV.host + '/api-token-auth/', //'/api-auth-basic/',
-        type: 'POST',
-        data: JSON.stringify({
-          username: username,
-          password: password
-        }),
-        contentType: 'application/json', //'application/json;charset=utf-8',
-        dataType: 'json'
-      }).then((response) => {
-        Ember.run(function() {
-          resolve({
-            token: response.data.token
-          });
+    restore(data) {
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            if (!Ember.isEmpty(data.token)) {
+                resolve(data);
+            } else {
+                reject();
+            }
         });
-      }, (xhr, status, error) => {
-        console.log(error);
-        var response = xhr.responseText;
-        Ember.run(function() {
-          reject(response);
+    },
+    authenticate(username, password) {
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            Ember.$.ajax({
+                url: ENV.host + '/api-token-auth/', //'/api-auth-basic/',
+                type: 'POST',
+                data: JSON.stringify({
+                username: username,
+                password: password
+            }),
+                contentType: 'application/json', //'application/json;charset=utf-8', //'application/vnd.api+json',
+                dataType: 'json'
+            }).then((response) => {
+                Ember.run(function() {
+                    resolve({
+                        token: response.token //response.data.token
+                    });
+                });
+            }, (xhr, status, error) => {
+                console.log(error);
+                var response = xhr.responseText;
+                Ember.run(function() {
+                    reject(response);
+                });
+            });
         });
-      });
-    });
-  },
+    },
 });
--- app/routes/admin/categories/new.js
+++ app/routes/admin/categories/new.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        return this.store.createRecord('category');
+    },
+
+    actions: {
+        saveCategory(newCategory) {
+            newCategory.save().then(() => this.transitionTo('categories'));
+        },
+
+        willTransition() {
+            this.controller.get('model').rollbackAttributes();
+        }
+    }
 });
