35857895c510929c1d4077bba2943e4133e8ebf3
Mahmood Khan <mkhan@mercycorps.org>
2017-01-26 07:51:56 -0800

    added sorting to the assets table

---
+++ app/controllers/assets/index.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    sortedAssets: Ember.computed.sort('model', 'sortDefinition'),
+    //sortBy: 'no:desc',
+    sortDefinition: ['no'],
+    actions: {
+        sortBy: function(property) {
+            console.log(property);
+            this.set('sortDefinition', [property]);
+        }
+    },
+    /*
+    sortDefinition: Ember.computed('sortBy', function() {
+        return [ this.get('sortBy') ];
+    }),
+    */
+});
