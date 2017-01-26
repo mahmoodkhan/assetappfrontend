6dfc6c096d3c41cfdd790a382718681c557ddb7f
Mahmood Khan <mkhan@mercycorps.org>
2017-01-26 07:48:48 -0800

    coalesce all related object calls into single ajax call

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,5 +6,16 @@ import ENV from 'frontendapp/config/environment';
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
     host: ENV.host,
     namespace: 'api',
+
+    /* By default the JSONAPIAdapter will send each find request coming from a
+     * store.find or from accessing a relationship separately to the server.
+     * If your server supports passing ids as a query string, you can set
+     * coalesceFindRequests to true to coalesce all find requests within a
+     * single runloop. */
+    coalesceFindRequests: true,
+
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+    },
     authorizer: 'authorizer:drf-token-authorizer',
 });
--- config/environment.js
+++ config/environment.js
@@ -21,6 +21,8 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      usingCors: true,
+      corsWithCreds: true,
     },
     apiURL: '/api',
   };
