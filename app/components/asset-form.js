e1ff6e60f011867e8971c0edad5dab7fa377217c
Mahmood Khan <mkhan@mercycorps.org>
2017-01-31 13:06:43 -0800

    got ember-power-select to show countries from store

--- app/components/asset-form.js
+++ app/components/asset-form.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    countries: '',
+    store: Ember.inject.service(),
+    didInsertElement: function(){
+        this.set('countries', this.get('store').findAll('country'));
+    },
     actions: {
-        foo: function(country) {
-            this.set('country', country);
-            console.log(country.id);
-        }
+        country_change: function(country) {
+            this.set('selected_country', country);
+            console.log(country);
+        },
     },
 });
