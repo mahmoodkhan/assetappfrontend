2dca866f0da5a6e013f6cc7608edb07a867018aa
Mahmood Khan <mkhan@mercycorps.org>
2017-03-13 21:10:33 -0700

    added sorting by fields

--- app/controllers/assets/index.js
+++ app/controllers/assets/index.js
@@ -2,17 +2,41 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     sortedAssets: Ember.computed.sort('model', 'sortDefinition'),
-    //sortBy: 'no:desc',
-    sortDefinition: ['no'],
-    actions: {
-        sortBy: function(property) {
-            //console.log(property);
-            this.set('sortDefinition', [property]);
+    sortDefinition: ['no:asc'],
+    sortAscending: true,
+
+    sortHelper: function(field) {
+        let sortDefinition = this.get('sortDefinition')[0].split(':')[0];
+        console.log(sortDefinition);
+        console.log(field);
+        if (sortDefinition === field) {
+            if (this.get('sortAscending') === true) {
+                return 'sorted glyphicon  glyphicon-arrow-down';
+            } else {
+                return 'sorted glyphicon glyphicon-arrow-up';
+            }
+        } else {
+            return 'glyphicon glyphicon-sort';
         }
     },
-    /*
-    sortDefinition: Ember.computed('sortBy', function() {
-        return [ this.get('sortBy') ];
+
+    sortByNo: Ember.computed('sortDefinition', function() {
+        return this.sortHelper('no');
     }),
-    */
+
+    sortByDescription: Ember.computed('sortDefinition', function() {
+        return this.sortHelper('description');
+    }),
+
+    sortByCategory: Ember.computed('sortDefinition', function() {
+        return this.sortHelper('category.category');
+    }),
+
+    actions: {
+        sortBy: function(property) {
+            let sortOrder = this.get('sortAscending') ? 'desc' : 'asc';
+            this.set('sortDefinition', [`${property}:${sortOrder}`]);
+            this.set('sortAscending', !this.sortAscending);
+        },
+    },
 });
