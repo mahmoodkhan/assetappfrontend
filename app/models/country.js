b032d84c170f9ee1bb68f2f766d52663d1f7298f
Mahmood Khan <mkhan@mercycorps.org>
2017-03-22 20:20:39 -0700

    fixed an inverse relationship

--- app/models/country.js
+++ app/models/country.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
     name: DS.attr('string'),
     iso2: DS.attr('string'),
     region: DS.belongsTo('region', {inverse: 'countries', async: false}),
-    assets: DS.hasMany('asset', {inverse: 'countries', async: false}),
-    offices: DS.hasMany('office', {inverse: 'countries', async: false}),
-    custodians: DS.hasMany('custodian', {inverse: 'countries', async: false}),
+    assets: DS.hasMany('asset', {inverse: 'country', async: false}),
+    offices: DS.hasMany('office', {inverse: 'country', async: false}),
+    custodians: DS.hasMany('custodian', {inverse: 'country', async: false}),
 });
